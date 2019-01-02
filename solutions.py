57c8e5a015bb073a78dcbca6369a650b5fd791ff
MikeChunko <chunkomike@gmail.com>
2019-01-01 19:02:50 -0500

    Solved problem 18 (and problem 67)

--- solutions.py
+++ solutions.py
@@ -326,7 +326,39 @@ def problem_17(n):
     return running_total
 
 
+def problem_18():
+    """ Return the maximum total obtained by starting at the top of the given triangle
+        and moving adjacent rows below, summing each number in the process.
+        Use a bottom-up "greedy" algorithm to solve the problem efficiently.
+        The exact same algorithm is used for problem 67. """
+    triangle = [[75]]
+    triangle += [[95, 64]]
+    triangle += [[17, 47, 82]]
+    triangle += [[18, 35, 87, 10]]
+    triangle += [[20, 4, 82, 47, 65]]
+    triangle += [[19, 1, 23, 75, 3, 34]]
+    triangle += [[88, 2, 77, 73, 7, 63, 67]]
+    triangle += [[99, 65, 4, 28, 6, 16, 70, 92]]
+    triangle += [[41, 41, 26, 56, 83, 40, 80, 70, 33]]
+    triangle += [[41, 48, 72, 33, 47, 32, 37, 16, 94, 29]]
+    triangle += [[53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14]]
+    triangle += [[70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57]]
+    triangle += [[91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48]]
+    triangle += [[63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31]]
+    triangle += [[4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
+    print(triangle)
+
+    # Go from the n - 1th row to the 1st row
+    for i in range(len(triangle) - 2, -1, -1):
+        # Replace the value of triangle[i][j] with the max possible value obtained from summing
+        # triangle[i][j] with the higher of the two values adjacent and below it.
+        for j in range(0, len(triangle[i])):
+            triangle[i][j] = triangle[i][j] + max(triangle[i + 1][j: j + 2])
+
+    return triangle[0][0]
+
+
 if __name__ == "__main__":
     start_time = time.time()
-    print(problem_17(1000))
+    print(problem_18())
     print("--- %s seconds ---" % (time.time() - start_time))
