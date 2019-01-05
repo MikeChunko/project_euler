f77f7debd44edaed05d3ee848d5a86f9e77bb350
MikeChunko <chunkomike@gmail.com>
2019-01-05 18:55:54 -0500

    Initial commit

---
+++ solutions_21-40.py
@@ -0,0 +1,10 @@
+# File name: solutions_21-40.py
+# Author: Michael Chunko
+# Date created: 1/5/19
+# Python Version: 3.7
+
+# This file contains the algorithms used for solving problems 21 through 40 (inclusive) from projecteuler.net
+
+import math
+import time
+import sys
