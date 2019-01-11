23d39efd7e68558beae2129ed7f50cd4ca4baefe
MikeChunko <chunkomike@gmail.com>
2019-01-11 12:16:59 -0500

    Created a function for finding primes

--- helpers.py
+++ helpers.py
@@ -8,3 +8,24 @@
 import math
 import time
 import sys
+
+
+def eratosthenes_sieve(n):
+    """ Return all prime numbers <= n using the Sieve of Eratosthenes. """
+    primes = [1]
+    possible_primes = list(range(2, n + 1))
+
+    while possible_primes:
+        primes += [possible_primes[0]]
+
+        i = 0
+        current_prime = primes[-1]
+        while i < len(possible_primes):
+
+            if possible_primes[i] % current_prime == 0:
+                possible_primes.remove(possible_primes[i])
+                i += current_prime - 1
+
+            i += 1
+
+    return primes
