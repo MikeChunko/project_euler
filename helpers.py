2196bc05c1f46a1cc7bc0043c06315111c39d353
MikeChunko <chunkomike@gmail.com>
2019-01-11 12:16:43 -0500

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,10 @@
+# File name: solutions_21-40.py
+# Author: Michael Chunko
+# Date created: 1/5/19
+# Python Version: 3.7
+
+# This file contains some common helper algorithms used in solving various problem
+
+import math
+import time
+import sys
